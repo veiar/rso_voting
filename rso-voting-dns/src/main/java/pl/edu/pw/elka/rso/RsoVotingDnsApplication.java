ea549ae073e02092c9c3fb4b41312f006613e91e
Artur Gwiazdowicz <agwiazdo@mion.elka.pw.edu.pl>
2016-05-05 00:39:33 +0200

    Zaczyn serwisu dnsowego

---
+++ rso-voting-dns/src/main/java/pl/edu/pw/elka/rso/RsoVotingDnsApplication.java
@@ -0,0 +1,12 @@
+package pl.edu.pw.elka.rso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsoVotingDnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsoVotingDnsApplication.class, args);
+	}
+}
