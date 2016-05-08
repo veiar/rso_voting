7aab217d88055e5cea54f74ef552a6e746758218
grudzien.e <grudzien.e@gmail.com>
2016-05-08 20:43:17 +0200

    adding canvasJs and pie chart

--- rso-web2py/applications/RSO/static/js/rso.js
+++ rso-web2py/applications/RSO/static/js/rso.js
@@ -1,9 +1,11 @@
-$(document).ready(function() {setupGraph()});
+$(document).ready(function() {showColumnChart()});
 
-function setupGraph()
+function showColumnChart()
 {
-    var ctx = document.getElementById("myChart");
-    var myChart = new Chart(ctx, {
+    document.getElementById("pieChart").style.visibility='hidden';
+    document.getElementById("columnChart").style.visibility='visible';
+    var ctx = document.getElementById("columnChart");
+    var columnChart = new Chart(ctx, {
     type: 'bar',
     data: {
         labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
@@ -22,4 +24,43 @@ function setupGraph()
         }
     }
 });
+}
+
+function ShowPieChart()
+{
+    document.getElementById("columnChart").style.visibility='hidden';
+    document.getElementById("pieChart").style.visibility='visible';
+    var chart = new CanvasJS.Chart("pieChart",
+	{
+		title:{
+			text: "Voting results"
+		},
+                animationEnabled: true,
+		legend:{
+			verticalAlign: "top",
+			horizontalAlign: "left",
+			fontSize: 20,
+			fontFamily: "Helvetica"        
+		},
+		theme: "theme2",
+		data: [
+		{        
+			type: "pie",       
+			indexLabelFontFamily: "Garamond",       
+			indexLabelFontSize: 20,
+			indexLabel: "{label} {y}%",
+			startAngle:-20,      
+			showInLegend: true,
+			toolTipContent:"{legendText} {y}%",
+			dataPoints: [
+				{  y: 83.24, legendText:"Red", label: "Google" },
+				{  y: 8.16, legendText:"Blue", label: "Blue" },
+				{  y: 4.67, legendText:"Yellow", label: "Yellow" },
+				{  y: 1.67, legendText:"Green" , label: "Green"},       
+				{  y: 0.98, legendText:"Purple" , label: "Purple"}
+			]
+		}
+       ]
+	});
+	chart.render();
 }
