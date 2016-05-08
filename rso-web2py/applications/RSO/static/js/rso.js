c225c1cb195fc710e9e16d5959121810150edaa8
rafal <dzumagos@gmail.com>
2016-05-08 16:24:31 +0200

    added Chart.js

---
+++ rso-web2py/applications/RSO/static/js/rso.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {setupGraph()});
+
+function setupGraph()
+{
+    var ctx = document.getElementById("myChart");
+    var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3]
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+}
