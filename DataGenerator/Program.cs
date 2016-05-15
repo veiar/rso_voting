f9e7cc75caedf3f56f2a67adb106f50c4e0884e0
grudzien.e <grudzien.e@gmail.com>
2016-05-15 18:52:58 +0200

    adding DataGenerator

---
+++ DataGenerator/Program.cs
@@ -0,0 +1,60 @@
+﻿using FizzWare.NBuilder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net;
+using System.IO;
+using System.Configuration;
+
+namespace DataGenerator
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            VoteRepositoryGenerator _voteRepository = new VoteRepositoryGenerator();
+            
+            string url = ConfigurationManager.ConnectionStrings["URL"].ConnectionString;
+            int votesNumberPerPackage = int.Parse(ConfigurationManager.AppSettings["votesNumberPerPackage"]); 
+            int packageNumber = 1; //TODO
+            int interval = 0; //TODO
+
+            var json = JsonConvert.SerializeObject(_voteRepository.GetRandomData(votesNumberPerPackage));
+            //Console.WriteLine(json);
+
+            Console.WriteLine(string.Format(" **Data generator for RSO_Voting Project 2016** \n ConnectionString: {0}, \n Number of votes per package: {1}, \n Number of packages: {2}, Interval: {3}.\n\n",
+                url, votesNumberPerPackage, packageNumber, interval));
+
+            try
+            {
+                var request = (HttpWebRequest)WebRequest.Create(url);
+                var postData = json;
+                var data = Encoding.ASCII.GetBytes(postData);
+
+                request.Method = "POST";
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.ContentLength = data.Length;
+
+                using (var stream = request.GetRequestStream())
+                {
+                    stream.Write(data, 0, data.Length);
+                }
+
+                var response = (HttpWebResponse)request.GetResponse();
+
+                var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                Console.WriteLine(string.Format("Connected to {0}. /n All votes sended", url));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(string.Format("Unable to connect to {0}. Error: {1}", url, ex));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DataGenerator/VotingData.cs
@@ -0,0 +1,78 @@
+﻿using FizzWare.NBuilder;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Text.RegularExpressions;
+
+namespace DataGenerator
+{
+    public class VoteEntity
+    {
+        public string Pesel { get; set; }
+        public string Vote { get; set; }
+        public string VotingArea { get; set; }
+        public Gender Gender { get; set; }
+        public string Address { get; set; }
+        public string Education { get; set; }
+    }
+
+    public class VoteRepository
+    {
+        private IList<VoteEntity> _votes;
+
+        public IList<VoteEntity> Votes
+        {
+            get { return _votes; }
+            set { _votes = value; }
+        }
+
+        public IList<VoteEntity> GetAll()
+        {
+            return _votes;
+        }
+
+        public VoteEntity Get(string pesel)
+        {
+            return _votes.Single(p => p.Pesel == pesel);
+        }
+
+        public IList<VoteEntity> GetByGender(Gender gender)
+        {
+            return _votes.Where(p => p.Gender == gender).ToList();
+        }
+
+    }
+
+    public class VoteRepositoryGenerator
+    {
+        private VoteRepository _voteRepository = new VoteRepository();
+        public IList<VoteEntity> GetRandomData(int numberOfVotes)
+        {
+            IList<VoteEntity> votes = Builder<VoteEntity>.CreateListOfSize(numberOfVotes)
+                .All()
+                    .With(p => p.Pesel = GetPesel())
+                    .With(c => c.Address = Faker.Address.StreetAddress() + ", " + Faker.Address.City())
+                .Build();
+
+            _voteRepository.Votes = votes.ToList();
+            IList<VoteEntity> result = _voteRepository.GetAll();
+            return result;
+        }
+
+        private string GetPesel()
+        {
+            var numGenerator = new RandomGenerator();
+            string month = numGenerator.DateTime().Month.ToString();
+            if (month.Length == 1) month = "0" + month;
+            string year = numGenerator.Next(20, 98).ToString();
+            string day = numGenerator.DateTime().Day.ToString();
+            if (day.Length == 1) day = "0" + month;
+
+            return year + month + day + numGenerator.Next(10000, 99999).ToString();
+        }
+    }
+}
