b9c2a034d1ac5962ee99aeb2cdc2356197efb21e
misiekk <misiowiecp@gmail.com>
2016-05-19 12:15:50 +0200

    aggregator - loading cfg from file v1

--- rso-aggregator/src/DBHandler/MongoHandler.java
+++ rso-aggregator/src/DBHandler/MongoHandler.java
@@ -5,15 +5,19 @@ import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
-import java.util.Arrays;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.*;
+
 
 //db.createUser({"user": "pm", "pwd": "pass123", roles: ["readWrite", "dbAdmin"]})
 public class MongoHandler {
-    private static String user = "pm";
-    private static char[] pass = "pass123".toCharArray();
-    private static String dbName = "testDB";
+    private String user;// = "pm";
+    private char[] pass;// = "pass123".toCharArray();
+    private String dbName;// = "testDB";
     private MongoClient mongoClient;
     private MongoDatabase mongoDB;
+    Map<String, List<String>> propertiesMap;
     public MongoHandler(){
         init();
         //test();
@@ -21,12 +25,35 @@ public class MongoHandler {
     }
 
     private void init(){
+        try {
+            getProperties();
+        }
+        catch (IOException e){
+            System.err.println("Loading from cfg file failed, exiting!");
+            System.exit(1);
+        }
         MongoCredential cred = MongoCredential.createCredential(user, dbName, pass);
         //this.mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
         this.mongoClient = new MongoClient(new ServerAddress(), Arrays.asList(cred));
         this.mongoDB = mongoClient.getDatabase(dbName);
     }
 
+    private void getProperties() throws IOException{
+        java.util.Properties properties = new Properties();
+        properties.load(new FileInputStream("cfg/aggregatorMongo.cfg"));
+        this.propertiesMap = new HashMap<>();
+        Set<String> data = properties.stringPropertyNames();
+        for(String key : data){
+            propertiesMap.put(key, Arrays.asList(properties.getProperty(key).split(",")));
+        }
+
+        this.user = propertiesMap.get("mongoUser2").get(0);
+        this.pass = propertiesMap.get("mongoPass2").get(0).toCharArray();
+        this.dbName = propertiesMap.get("mongoDBName2").get(0);
+        /*this.user = properties.getProperty("mongoUser");
+        this.pass = properties.getProperty("mongoPass").toCharArray();
+        this.dbName = properties.getProperty("mongoDBName");*/
+    }
     public void close(){
         this.mongoClient.close();
     }
