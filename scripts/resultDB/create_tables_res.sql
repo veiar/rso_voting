efb0089aba8b673577d26455c146bd9a27fbf46e
misiekk <misiowiecp@gmail.com>
2016-05-22 20:52:01 +0200

    poprawka skryptu

--- scripts/resultDB/create_tables_res.sql
+++ scripts/resultDB/create_tables_res.sql
@@ -5,7 +5,7 @@ CREATE TABLE res_party_age
 (
   party_id integer NOT NULL,
   age_id integer NOT NULL,
-  percentage double precision NOT NULL,
+  votes_sum integer NOT NULL,
   CONSTRAINT pk_res_pa PRIMARY KEY (party_id, age_id),
   CONSTRAINT fk_res_pa_age_id FOREIGN KEY (age_id)
       REFERENCES d_age (age_id) MATCH SIMPLE
@@ -20,18 +20,15 @@ WITH (
 ALTER TABLE res_party_age
   OWNER TO postgres;
 
+
 -- % glosow na kandydatow z poszczegolnych partii
 CREATE TABLE res_party_candidates
 (
-  party_id integer NOT NULL,
   candidate_id integer NOT NULL,
-  percentage double precision NOT NULL,
-  CONSTRAINT pk_res_party_candidates PRIMARY KEY (party_id, candidate_id),
-  CONSTRAINT fk_res_party_cand_candidate_id FOREIGN KEY (candidate_id)
+  votes_sum integer NOT NULL,
+  CONSTRAINT pk_res_party_candidates PRIMARY KEY (candidate_id),
+  CONSTRAINT fk_res_party_candidates FOREIGN KEY (candidate_id)
       REFERENCES d_candidates (candidate_id) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION,
-  CONSTRAINT fk_res_party_cand_party_id FOREIGN KEY (party_id)
-      REFERENCES d_parties (party_id) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE NO ACTION
 )
 WITH (
@@ -40,12 +37,13 @@ WITH (
 ALTER TABLE res_party_candidates
   OWNER TO postgres;
 
+
 -- % glosow na partie w poszczegolnych okregach
 CREATE TABLE res_party_constituency
 (
   party_id integer NOT NULL,
   constituency_id integer NOT NULL,
-  percentage double precision,
+  votes_sum integer,
   CONSTRAINT pk_res_party_constituency PRIMARY KEY (party_id, constituency_id),
   CONSTRAINT fk_res_pc_constituency_id FOREIGN KEY (constituency_id)
       REFERENCES d_constituencies (constituency_id) MATCH SIMPLE
@@ -65,7 +63,7 @@ CREATE TABLE res_party_education
 (
   party_id integer NOT NULL,
   education_id integer NOT NULL,
-  percentage double precision NOT NULL,
+  votes_sum integer NOT NULL,
   CONSTRAINT pk_res_pe PRIMARY KEY (party_id, education_id),
   CONSTRAINT fk_res_pe_edu_id FOREIGN KEY (education_id)
       REFERENCES d_education (education_id) MATCH SIMPLE
@@ -81,11 +79,11 @@ ALTER TABLE res_party_education
   OWNER TO postgres;
 
 
---% glosow na parie ogolem
+--% glosow na partie ogolem
 CREATE TABLE res_party_percent
 (
   party_id integer NOT NULL,
-  percentage double precision NOT NULL,
+  votes_sum integer NOT NULL,
   CONSTRAINT pk_res_party_percent PRIMARY KEY (party_id),
   CONSTRAINT fk_res_party_percent FOREIGN KEY (party_id)
       REFERENCES d_parties (party_id) MATCH SIMPLE
@@ -104,7 +102,7 @@ CREATE TABLE res_party_sex
 (
   party_id integer NOT NULL,
   sex_id integer NOT NULL,
-  percentage double precision NOT NULL,
+  votes_sum integer NOT NULL,
   CONSTRAINT pk_res_ps PRIMARY KEY (party_id, sex_id),
   CONSTRAINT fk_res_ps_party_id FOREIGN KEY (party_id)
       REFERENCES d_parties (party_id) MATCH SIMPLE
@@ -118,4 +116,3 @@ WITH (
 );
 ALTER TABLE res_party_sex
   OWNER TO postgres;
-
