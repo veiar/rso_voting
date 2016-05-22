0dcdabe91b313a375cb0d3228557af316012297f
rafal <dzumagos@gmail.com>
2016-05-22 14:40:19 +0200

    fixes

--- rso-web2py/parameters_8000.py
+++ rso-web2py/parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$990bd19e3631754e$be6d8f31306fc3683d9041bea22ba30e146170da"
+password="pbkdf2(1000,20,sha512)$8dbe49d581db3c6f$bc709fad99aa522a6dd1f43f2f06ae3e31a7f1d3"
