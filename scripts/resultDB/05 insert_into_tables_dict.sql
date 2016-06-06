86ea13e5715131c9bf1d133372f278fef6e3a154
grudzien.e <grudzien.e@gmail.com>
2016-06-06 19:29:29 +0200

    initial data changes

--- scripts/resultDB/05 insert_into_tables_dict.sql
+++ scripts/resultDB/05 insert_into_tables_dict.sql
@@ -1,5 +1,16 @@
--- EMPTY ALL DICT TABLES
--- TRUNCATE d_parties CASCADE
+-- EMPTY ALL DICT TABLES AND RESTART SEQUENCE
+TRUNCATE d_age CASCADE;
+TRUNCATE d_candidates CASCADE;
+TRUNCATE d_constituencies CASCADE;
+TRUNCATE d_education CASCADE;
+TRUNCATE d_parties CASCADE;
+TRUNCATE d_sex CASCADE;
+
+ALTER SEQUENCE seq_d_age RESTART WITH 1;
+ALTER SEQUENCE seq_d_parties RESTART WITH 1;
+ALTER SEQUENCE seq_d_parties RESTART WITH 1;
+ALTER SEQUENCE seq_d_candidates WITH 1;
+ALTER SEQUENCE seq_d_constituencies WITH 1;
 
 --
 -- Data for Name: d_age; Type: TABLE DATA; Schema: public; Owner: postgres
