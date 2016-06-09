b3e1baaa22e06048f0481f36b1be79491e7e1ad3
misiekk <misiowiecp@gmail.com>
2016-06-09 17:24:28 +0200

    aggregator - multiple mongo db connection handling v7`

--- rso-aggregator/src/Main.java
+++ rso-aggregator/src/Main.java
@@ -1,11 +1,29 @@
 import DBHandler.ConnectionHandler;
 import RsoAggregator.RsoAggregator;
 
+import java.util.Map;
+import java.util.logging.Level;
+
+import static RsoAggregator.RsoAggregator.logger;
+
 public class Main {
 
     public static void main(String[] args) {
         boolean master = true;
-        try {
+        Map<String, String> env = System.getenv();
+        if(!env.containsKey("MASTER")){
+            System.err.println("No master environment variables set! Bye!");
+            logger.log(Level.SEVERE, "No environment variables set! Bye!");
+            System.exit(2);
+        }
+        String isMaster = env.get("MASTER");
+        if(isMaster.equals("1")){
+            master = true;
+        }
+        else{
+            master = false;
+        }
+        /*try {
             Integer arg = Integer.parseInt(args[0]);
             System.out.println(arg);
             if (arg == 0) {
@@ -17,7 +35,7 @@ public class Main {
             System.err.println("No parameters! Bye!");
 
             //System.exit(2);
-        }
+        }*/
 
         final RsoAggregator rsoAggregator = new RsoAggregator(master);
 
